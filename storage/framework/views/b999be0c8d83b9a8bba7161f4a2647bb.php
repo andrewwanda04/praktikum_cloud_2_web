bd069fd28e7c5fe8df661920885d9c39ed100f7a
Andrew Wanda <andrewwanda94@gmail.com>
2025-10-21 19:12:05 +0700

    Add files via upload

---
+++ storage/framework/views/b999be0c8d83b9a8bba7161f4a2647bb.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Test Andrew</title>
+    <!-- Bootstrap 5 CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light d-flex align-items-center justify-content-center vh-100">
+    <div class="text-center">
+        <h1 class="fw-bold text-primary display-3">Halo Aku Andrew Wanda</h1>
+        <p class="text-muted fs-5 mt-3">Andrew Wanda Bagaskara</p>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+<?php /**PATH C:\Users\andre\Kuliah\Semester 5\Komputasi Awan\praktikum2-web\resources\views/welcome.blade.php ENDPATH**/ ?>
